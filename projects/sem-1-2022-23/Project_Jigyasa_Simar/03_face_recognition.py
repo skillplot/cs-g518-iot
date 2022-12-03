770438eddfd84938f4d7e049cbf33dbf8ed257d3
Jigyasa Agarwal <41884509+jigyasa1234@users.noreply.github.com>
2022-12-03 06:35:17 +0530

    Add files via upload

---
+++ projects/sem-1-2022-23/Project_Jigyasa_Simar/03_face_recognition.py
@@ -0,0 +1,93 @@
+''''
+Real Time Face Recogition
+	==> Each face stored on dataset/ dir, should have a unique numeric integer ID as 1, 2, 3, etc                       
+	==> LBPH computed model (trained faces) should be on trainer/ dir
+Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
+
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18  
+
+'''
+
+import cv2
+import numpy as np
+import os 
+import re
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+#iniciate id counter
+id = 0
+
+# names related to ids: example ==> Marcelo: id=1,  etc
+with open("names.txt", "r") as f:
+    name=f.read()
+names=name.splitlines()
+
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture(0)
+cam.set(3, 640) # set video widht
+cam.set(4, 480) # set video height
+
+# Define min window size to be recognized as a face
+minW = 0.1*cam.get(3)
+minH = 0.1*cam.get(4)
+copy_id=-1
+while True:
+
+    ret, img =cam.read()
+    img = cv2.flip(img, -1) # Flip vertically
+
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+       )
+
+    for(x,y,w,h) in faces:
+
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        # Check if confidence is less them 100 ==> "0" is perfect match 
+        if (confidence < 100):
+            id = names[id]
+            confidence = "  {0}%".format(round(100 - confidence))
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
+        
+        s = ''.join(x for x in confidence if x.isdigit())
+        if (int(s)<30):
+            id= "unknown"
+        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+        cv2.putText(img, confidence, (x+5,y+h-5), font, 1, (255,255,0), 1) 
+        if(copy_id!=id):
+            os.popen ( f'espeak "{id}" --stdout | aplay 2>/dev/null')
+        if(copy_id==-1 or copy_id!=id):
+            copy_id=id
+        
+        '''if (id=="unknown"):
+            os.system("espeak 'This is a new face. Do you want to store it? Yes or No'")
+            answer=input('\n This is a new face. Do you want to store it?(Yes/No)')
+            if(answer=="Yes"):
+                import face_dataset'''
+         
+    
+    cv2.imshow('camera',img) 
+
+    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
+
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
